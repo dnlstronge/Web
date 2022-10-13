663d356af27578667246c72625a69049899ca473
Dan Stronge <dnlstronge@gmail.com>
2022-10-13 14:23:25 +0100

    create: frame of code for random message generator

---
+++ exercise.js
@@ -0,0 +1,5 @@
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+const greetAliens = aliens => {
+  for(let i = 0; i < aliens.length; i++) {return "Oh powerful "  " We humans offer our unconditional surrender!" }
+}
---
+++ randomMessageGenerator.js
@@ -0,0 +1,9 @@
+var words1 = ['Greetings!', 'Well, hello', 'Hola!', 'Hail friend', 'Well met, ', 'Goodday']
+var words2 = ['There are strange things afoot they say', 'Have you heard about what when on them mines?',
+              'I hear there be a carnival setting up down by the coast', 'Too many strangers in these parts as of late'              
+]
+var words3 = []
+
+var rand1 = Math.floor(Math.random * words2.length);
+var rand2 = Math.floor(Math.random * words2.length);
+var rand3 = Math.floor(Math.random * words2.length);
