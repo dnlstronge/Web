c994a9b959f42921ce3414650090c72f87b79e01
Dan Stronge <dnlstronge@gmail.com>
2022-10-17 11:11:37 +0100

    chore: folder tidy

--- blog.js
+++
@@ -1,3 +0,0 @@
-const testing = document.getElementById("test");
-testing.innerHTML = "Had a lousy day bird-watching, it did nothing but rain";
-console.log(testing.innerHTML);
--- exercise.js
+++
@@ -1,6 +0,0 @@
-unction init() {
-  const testing = document.getElementById("test");
-  testing.innerHTML = "Had a lousy day bird-watching, it did nothing but rain";
-  testing.setAttribute("class", "redText");
-  }
-  window.onload = init;
